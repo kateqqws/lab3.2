36498e751158f791c0a11eb3f84afab4f2619970
unknown <katepob160@gmail.com>
2023-10-05 22:17:29 +0300

    lab 3.2 commit

---
+++ Lab3.2/Lab3.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+    double x;
+    double f;
+
+    cout << "Enter a = "; cin >> a;
+    cout << "\nEnter b = "; cin >> b;
+    cout << "\nEnter c = "; cin >> c;
+    cout << "\nEnter x = "; cin >> x;
+
+    if ((x < 0) && (b != 0))
+        f = -((2 * x - c) / (c * x - a));
+    else
+        if ((x > 0) && (b == 0))
+            f = (x - a) / (x - c);
+        else
+            f = -(x / c) + (-c / 2 * x);
+
+    cout << "\nF = " << f;
+}
+
---
+++ Lab3.2/Lab3.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+    double x;
+    double f;
+
+    cout << "Enter a = "; cin >> a;
+    cout << "\nEnter b = "; cin >> b;
+    cout << "\nEnter c = "; cin >> c;
+    cout << "\nEnter x = "; cin >> x;
+
+    if ((x < 0) && (b != 0))
+        f = -((2 * x - c) / (c * x - a));
+    if ((x > 0) && (b == 0))
+        f = (x - a) / (x - c);
+    if (!((x < 0) && (b != 0)) && !((x > 0) && (b == 0)))
+        f = -(x / c) + (-c / 2 * x);
+
+    cout << "\nF1 = " << f;
+    
+    if ((x < 0) && (b != 0))
+        f = -((2 * x - c) / (c * x - a));
+    else
+        if ((x > 0) && (b == 0))
+            f = (x - a) / (x - c);
+        else
+            f = -(x / c) + (-c / 2 * x);
+
+    cout << "\nF2 = " << f;
+}
